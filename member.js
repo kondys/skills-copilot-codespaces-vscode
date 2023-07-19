0fdf72c164ed29e04e229efa25d599e71fbf87aa
kondys <53384806+kondys@users.noreply.github.com>
2023-07-19 03:21:07 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember(){
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var contact = document.getElementById("contact");
+    member.style.display = "block";
+    skills.style.display = "none";
+    contact.style.display = "none";
+}
